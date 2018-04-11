04e1cedccf5da77f3e14dc4526f715610c07b44c
Daniel Falbel <dfalbel@gmail.com>
2018-04-11 18:19:52 -0300

    working identically to python

--- src/RecordWriter.h
+++ src/RecordWriter.h
@@ -1,11 +1,26 @@
 #include <Rcpp.h>
 #include <fstream>
-#include <bitset>
-#include <zlib.h>
 #include "crc.hpp"
 using namespace Rcpp;
 
 // helper functions
+
+// some changes from:
+// https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/lib/core/coding.cc#L43
+void EncodeFixed64(char* buf, std::uint64_t value) {
+  memcpy(buf, &value, sizeof(value));
+}
+
+void EncodeFixed32(char* buf, std::uint32_t value) {
+  memcpy(buf, &value, sizeof(value));
+}
+
+// found implementation here: https://bidetly.io/2017/02/08/crc-part-1/
+std::uint32_t crc32_boost(const char* first, const char* last) {
+  boost::crc_optimal<32, 0x1edc6f41, 0xffffffff, 0xffffffff, true, true> machine;
+  machine.process_block(first, last);
+  return machine.checksum();
+}
 
 // mask delta constant
 // https://github.com/tensorflow/tensorflow/blob/754048a0453a04a761e112ae5d99c149eb9910dd/tensorflow/core/lib/hash/crc32c.h#L33
@@ -17,39 +32,8 @@ uint32_t mask(uint32_t crc) {
   return ((crc >> 15) | (crc << 17)) + mask_delta;
 };
 
-// https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/lib/core/coding.cc#L43
-void EncodeFixed64(char* buf, std::uint64_t value) {
-  memcpy(buf, &value, sizeof(value));
-}
-
-void EncodeFixed32(char* buf, std::uint32_t value) {
-  memcpy(buf, &value, sizeof(value));
-}
-
-// Get CRC32 rep
-// https://stackoverflow.com/a/39381287/3297472
-uint32_t crc32(const std::string& my_string) {
-  boost::crc_32_type result;
-  result.process_bytes(my_string.data(), my_string.length());
-  return result.checksum();
-};
-
-uint32_t crc32_uint(std::uint64_t x) {
-  boost::crc_32_type result;
-  auto x2 = static_cast<char*>(static_cast<void*>(&x));
-  result.process_bytes(x2, sizeof(x2));
-  return result.checksum();
-};
-
 std::uint32_t masked_crc (char * data, std::size_t n) {
-  boost::crc_32_type result;
-  result.process_bytes(data, n);
-  return mask(result.checksum());
-}
-
-std::string uint64toBytes (std::uint64_t x) {
-  std::bitset<sizeof(x)> bit(x);
-  return bit.to_string();
+  return mask(crc32_boost(data, data + n));
 }
 
 class RecordWriter {
@@ -81,34 +65,16 @@ public:
     char length_crc[sizeof(std::uint32_t)];
     EncodeFixed32(length_crc, masked_crc(length, sizeof(std::uint64_t)));
     
-    Rcout << sizeof(std::uint32_t) << "\n";
-    
-    // char header[sizeof(std::uint64_t) + sizeof(std::uint32_t)];
-    // EncodeFixed64(header + 0, data.size());
-    // EncodeFixed32(
-    //   header + sizeof(std::uint64_t), 
-    //   masked_crc(header, sizeof(std::uint64_t))
-    // );
-    // 
-    // char footer[sizeof(std::uint32_t)];
-    // EncodeFixed32(footer, mask(crc32(data)));
-    
-    // std::uint64_t length(sizeof(data));
-    // std::uint32_t lengthCRC = mask(crc32_uint(length));
-    // std::uint32_t dataCRC = mask(crc32(data));
-    
-    // Rcout << length << "\n";
-    // Rcout << uint64toBytes(length) << "\n";
-    // Rcout << data << "\n";
-    // Rcout << dataCRC << "\n";
+    char data_crc[sizeof(std::uint32_t)];
+    char * data_array = const_cast<char*>(data.c_str());
+    EncodeFixed32(data_crc, masked_crc(data_array, data.size()));
     
     this->writer.write(length, sizeof(length));
-    this->writer.write(length_crc, sizeof(length));
+    this->writer.write(length_crc, sizeof(length_crc));
     this->writer.write(data.c_str(), data.length());
-    // this->writer.write(footer, sizeof(footer));
-    // 
+    this->writer.write(data_crc, sizeof(data_crc));
+    
     return true;
   }
-  
   
 };
--- tests/testthat/test-compare to python.R
+++ tests/testthat/test-compare to python.R
@@ -11,6 +11,8 @@ test_that("writing correctly compared to python", {
   ref_file <- readr::read_file_raw(ref_file)
   gen_file <- readr::read_file_raw("example2.tfrecords")
   
+  
+  
   l_ref <- readBin(ref_file[1:8], what = "integer")
   l_gen <- readBin(gen_file[1:8], what = "integer")
   
@@ -18,7 +20,7 @@ test_that("writing correctly compared to python", {
   l_crc_gen <- readBin(gen_file[9:12], what = "integer")
   
   data_ref <- readBin(ref_file[13:(13 + l_ref-1)], what = "character")
-  data_gen <- readBin(gen_file[17:(17 + l_gen-1)], what = "character")
+  data_gen <- readBin(gen_file[13:(13 + l_gen-1)], what = "character")
   
   data_crc_ref <- readBin(tail(ref_file, 4), what = "integer")
   data_crc_gen <- readBin(tail(gen_file, 4), what = "integer")
@@ -26,6 +28,6 @@ test_that("writing correctly compared to python", {
   expect_equal(l_ref, l_gen)
   expect_equal(l_crc_ref, l_crc_gen)
   expect_equal(data_ref, data_gen)
-  
+  expect_equal(data_crc_ref, data_crc_gen)
 
 })
