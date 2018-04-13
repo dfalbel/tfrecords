4ccc5dee9844519dc1eb13eb01e4908d22592a17
Daniel Falbel <dfalbel@gmail.com>
2018-04-13 14:30:46 -0300

    reworking writing implementation

--- R/tfrecords.R
+++ R/tfrecords.R
@@ -2,10 +2,50 @@
 #' @importFrom Rcpp sourceCpp
 NULL
 
-.onUnload <- function (libpath) {
-  x <- tfrecord_shutdown()
-  library.dynam.unload("tfrecords", libpath)
+# .onUnload <- function (libpath) {
+#   x <- tfrecord_shutdown()
+#   library.dynam.unload("tfrecords", libpath)
+# }
+
+
+#' Write tfrecords from a list of data.
+#'
+#' @param data list of data.
+#'
+#' @export
+#' 
+#' @examples 
+#' data <- list(
+#'  x = matrix(1:1000, nrow = 100),
+#'  y = matrix(1:1000/2.1, nrow = 100),
+#'  z = Matrix::Matrix(sample(0:1, prob = c(0.9, 0.1), size = 1000, replace = TRUE), nrow = 100)
+#'  )
+#'
+#'
+write_tfrecords <- function (data, path) {
+  
+  desc <- lapply(data, get_class_and_type)
+  
+  write_tfrecords_(data, desc, path)
 }
 
+#' Get classes and value types from an object supported.
+#'
+#'
+get_class_and_type <- function (x) {
+  
+  k <- as.character(class(x))
+  
+  if (k == "matrix") {
+    type <- typeof(x)
+  } else if (k == "dgCMatrix") {
+    type <- typeof(x@x)
+  }
+  
+  list(
+    class = k,
+    type = type
+  )
+}
 
 
--- tests/testthat/test-write_tfrecord_matrix.R
+++ tests/testthat/test-write_tfrecord_matrix.R
@@ -2,30 +2,10 @@ context("write_tfrecord_matrix")
 
 library(tensorflow)
 library(tfdatasets)
+library(reticulate)
 
-if (fs::file_exists("example.tfrecords"))
-  fs::file_delete("example.tfrecords")
-
-test_that("writing tfrecord matrix works", {
-  
-  x <- matrix(1:1000, nrow = 100, ncol = 10)
-  tfrecords:::write_tfrecord(x, "example.tfrecords")
-  
-  parse_function <- function(example_proto) {
-    features = dict("x" = tf$FixedLenFeature(shape(10), tf$int64))
-    tf$parse_single_example(example_proto, features)
-  }
-  
-  df <- tfrecord_dataset("example.tfrecords")
-  df <- df %>% dataset_map(parse_function)
-  df <- df %>% dataset_batch(100)
-  batch <- next_batch(df)
-  sess <- tf$Session()
-  x_recovered <- sess$run(batch)$x
-  
-  expect_equal(x, x_recovered)
-  
-})
+os <- import("os")
+x <- os$environ$setdefault('TF_CPP_MIN_LOG_LEVEL', '3')
 
 if (fs::file_exists("examplelist.tfrecords"))
   fs::file_delete("examplelist.tfrecords")
@@ -35,7 +15,7 @@ test_that("writing a list of matrix", {
   x <- matrix(1:1000, nrow = 100, ncol = 10)
   y <- matrix(runif(1000), nrow = 100, ncol = 10)
   
-  tfrecords:::write_tfrecords(list(x = x, y = y), "examplelist.tfrecords")
+  tfrecords::write_tfrecords(list(x = x, y = y), "examplelist.tfrecords")
   
   parse_function <- function(example_proto) {
     features = dict(
@@ -45,7 +25,7 @@ test_that("writing a list of matrix", {
     tf$parse_single_example(example_proto, features)
   }
   
-  df <- tfrecord_dataset("examplelist.tfrecords")
+  suppressWarnings(suppressMessages(df <- tfrecord_dataset("examplelist.tfrecords")))
   df <- df %>% dataset_map(parse_function)
   df <- df %>% dataset_batch(100)
   batch <- next_batch(df)
@@ -53,5 +33,5 @@ test_that("writing a list of matrix", {
   x_recovered <- sess$run(batch)
   
   expect_equal(x, x_recovered$x)
-  expect_equivalent(y, x_recovered$y, tolerance = 2e-8) # it can be different because of floating points convertion
+  expect_equivalent(y, x_recovered$y, tolerance = 2e-7) # it can be different because of floating points convertion
 })
