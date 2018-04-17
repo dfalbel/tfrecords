65d5bbbaa4a16e3974c13b8cfa52058e1648276b
Daniel Falbel <dfalbel@gmail.com>
2018-04-17 01:50:11 -0300

    one more try to install

---
+++ tools/install-protobuf.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# install compiler
+# curl -OL https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
+# unzip protoc-3.5.1-linux-x86_64.zip -d protoc3
+# mv protoc3/bin/* /usr/local/bin/
+# mv protoc3/include/* /usr/local/include/
+
+# install runtime
+curl -OL https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-cpp-3.5.1.zip
+unzip protobuf-cpp-3.5.1.zip -d proto-run3
+cd proto-run3
+./configure
+make
+make check
+sudo make install
+sudo ldconfig # refresh shared library cache.
