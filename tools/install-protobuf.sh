df027b9eb35d22b0805f0586adfff5db0d9d57ff
Daniel Falbel <dfalbel@gmail.com>
2018-04-17 10:42:39 -0300

    ignore some folders

--- tools/install-protobuf.sh
+++ tools/install-protobuf.sh
@@ -8,8 +8,8 @@
 
 # install runtime
 curl -OL https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-cpp-3.5.1.zip
-unzip protobuf-cpp-3.5.1.zip -d proto-run3
-cd proto-run3/protobuf-3.5.1/
+unzip protobuf-cpp-3.5.1.zip -d proto
+cd proto/protobuf-3.5.1/
 ./configure
 make
 make check
