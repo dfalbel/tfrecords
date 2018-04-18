38f27a93bec7dbfe32b7d222466c5b4925cda68a
Daniel Falbel <dfalbel@gmail.com>
2018-04-18 19:22:31 -0300

    some tweaks

--- R/tfrecords.R
+++ R/tfrecords.R
@@ -27,13 +27,19 @@ NULL
 write_tfrecords <- function (data, path) {
   desc <- lapply(data, get_class_and_type)
   n_obs <- get_n_obs(data)
-  data <- lapply(data, function(x) {
+  data <- transpose_arrays(data)
+  invisible(write_tfrecords_(data, desc, n_obs, path))
+}
+
+#' Transpose arrays
+#' 
+transpose_arrays <- function(data) {
+  lapply(data, function(x) {
     if(is.array(x) & !is.matrix(x))
-       aperm(x, length(dim(x)):1)
+      aperm(x, length(dim(x)):1)
     else
       x
   })
-  invisible(write_tfrecords_(data, desc, n_obs, path))
 }
 
 #' Get classes and value types from an object supported.
