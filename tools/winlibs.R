33e04a48be4086706bbea73b9c7932ca36c6c867
Daniel Falbel <dfalbel@gmail.com>
2018-04-20 03:34:59 -0300

    switch to v3.5.1

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,7 +2,7 @@
 # by Jeroen Ooms. Last updated: August 2016
 if (!file.exists("../windows/protobuf-3.2.0/include/google/protobuf/descriptor.h")) {
   if (getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/protobuf/archive/v3.2.0.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/protobuf/archive/v3.5.1.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
