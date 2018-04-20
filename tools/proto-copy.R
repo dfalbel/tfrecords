8b78d7c815092a7b8fbaabd94b43c6db8f6de2dd
Daniel Falbel <dfalbel@gmail.com>
2018-04-20 04:01:05 -0300

    copying during configure

---
+++ tools/proto-copy.R
@@ -0,0 +1,4 @@
+file.copy("inst/proto/example.pb.cc", to = "src/example.pb.cc")
+file.copy("inst/proto/example.pb.h", to = "src/example.pb.h")
+file.copy("inst/proto/feature.pb.cc", to = "src/feature.pb.cc")
+file.copy("inst/proto/feature.pb.h", to = "src/feature.pb.h")
