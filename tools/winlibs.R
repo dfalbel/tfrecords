8aedba51a552c1a19efc476f83c8e0b49f51e5b0
Daniel Falbel <dfalbel@gmail.com>
2018-04-20 03:49:26 -0300

    copy to src dir

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -7,9 +7,9 @@ if (!file.exists("../windows/protobuf-3.5.1/include/google/protobuf/descriptor.h
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
   
-  file.copy("../inst/proto/example.pb.cc", to = "example.pb.cc")
-  file.copy("../inst/proto/example.pb.h", to = "example.pb.h")
-  file.copy("../inst/proto/feature.pb.cc", to = "feature.pb.cc")
-  file.copy("../inst/proto/feature.pb.h", to = "feature.pb.h")
+  file.copy("../inst/proto/example.pb.cc", to = "../src/example.pb.cc")
+  file.copy("../inst/proto/example.pb.h", to = "../src/example.pb.h")
+  file.copy("../inst/proto/feature.pb.cc", to = "../src/feature.pb.cc")
+  file.copy("../inst/proto/feature.pb.h", to = "../src/feature.pb.h")
   
 }
