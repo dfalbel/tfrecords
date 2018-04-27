2c67dddb022b63902c43f6b3ef6f05750af229ec
Daniel Falbel <dfalbel@gmail.com>
2018-04-27 12:44:10 -0300

    cleaning installation script

--- tools/install-protobuf.sh
+++ tools/install-protobuf.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-# install compiler
-# curl -OL https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
-# unzip protoc-3.5.1-linux-x86_64.zip -d protoc3
-# mv protoc3/bin/* /usr/local/bin/
-# mv protoc3/include/* /usr/local/include/
-
-# install runtime
+# install runtime and compiler on ubuntu for testing
 curl -OL https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-cpp-3.5.1.zip
 unzip protobuf-cpp-3.5.1.zip -d proto
 cd proto/protobuf-3.5.1/
